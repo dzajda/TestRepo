ee4f1f755948579ebba4adf8d4abc14546a1c7d1
Dominik <dominik.zajda@collectorpayments.fi>
2020-02-15 23:55:32 +0100

    Add files via upload

---
+++ HealthCheckExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Net.Mime;
+
+namespace Tesssst
+{
+    public static class HealthCheckExtensions
+    {
+        public static IEndpointConventionBuilder MapCustomHealthChecks(
+            this IEndpointRouteBuilder endpoints, string serviceName)
+        {
+            return endpoints.MapHealthChecks("/health", new HealthCheckOptions
+            {
+                ResponseWriter = async (context, report) =>
+                {
+                    var result = JsonConvert.SerializeObject(
+                        report.Entries.Select(e => e.Value.Description), 
+                        Formatting.None,
+                        new JsonSerializerSettings
+                        {
+                            NullValueHandling = NullValueHandling.Ignore
+                        });
+                    context.Response.ContentType = MediaTypeNames.Application.Json;
+                    await context.Response.WriteAsync(result);
+                }
+            });
+        }
+    }
+}
---
+++ StorageHealthCheck.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tesssst
+{
+    public class StorageHealthCheck : IHealthCheck
+    {
+        private readonly IConfiguration configuration;
+        private readonly ILogger<StorageHealthCheck> logger;
+
+        public StorageHealthCheck(ILogger<StorageHealthCheck> logger, IConfiguration configuration)
+        {
+            this.logger = logger;
+            this.configuration = configuration;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                var connectionString = configuration["StorageConnectionString"];
+                var containerName = configuration["StorageBlobContainer"];
+
+                var storageAccount = CloudStorageAccount.Parse(connectionString);
+                var blobClient = storageAccount.CreateCloudBlobClient();
+                var blobContainer = blobClient.GetContainerReference(containerName);
+
+                if (!await blobContainer.ExistsAsync())
+                {
+                    return HealthCheckResult.Unhealthy("dupa");
+                }
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e.Message);
+            }
+
+            logger.LogInformation("działa");
+            return HealthCheckResult.Healthy("działa");
+        }
+    }
+}
